4a5dc3a4581dfbd097c72003b324fdabc15a8eb7
Yohann DE <yohann.degli-esposti@epita.fr>
2020-08-03 14:47:39 +0200

    movement: Melody is moving !

---
+++ Assets/Scripts/DetectiveMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DetectiveMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        GroundHasBeenChecked();
+        Debug.Log(agent.velocity);
+        if (Mathf.Abs(agent.velocity.x) <= 1f && Mathf.Abs(agent.velocity.z) <= 1f)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+    }
+
+    private void GroundHasBeenChecked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject.tag == "Ground")
+                    agent.SetDestination(hit.point);
+            }
+        }
+    }
+}

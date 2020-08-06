f923e02962ba12676b7983ef1bc5b788550ee160
Yohann DE <yohann.degli-esposti@epita.fr>
2020-08-06 18:25:55 +0200

    lab: now done.

---
+++ Assets/Scripts/Float.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+
+    float refy;
+    bool isDescending = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        refy = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDescending)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+            if (transform.position.y >= refy + 2f)
+                isDescending = true;
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
+            if (transform.position.y <= refy - 2f)
+                isDescending = false;
+        }
+    }
+}
---
+++ Assets/Scripts/LightBubbleHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBubbleHandler : MonoBehaviour
+{
+    Light light;
+    float toadd = 0.01f;
+    private void Start()
+    {
+        light = GetComponentInChildren<Light>();
+    }
+
+    private void Update()
+    {
+
+        if (light.intensity <= 0f)
+            toadd = -toadd;
+        if (light.intensity >= 2f)
+            toadd = -toadd;
+
+        light.intensity += toadd * 2;
+    }
+}

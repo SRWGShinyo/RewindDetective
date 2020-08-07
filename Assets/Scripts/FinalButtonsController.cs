309c2c82ae3598e36c9a25e292d21a3df4a67982
Yohann DE <yohann.degli-esposti@epita.fr>
2020-08-07 10:36:58 +0200

    mcqbuttons: added a template to mcqbuttons.

---
+++ Assets/Scripts/FinalButtonsController.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalButtonsController : MonoBehaviour
+{
+    [Serializable]
+    public class ButtonQuizz
+    {
+        public List<string> sentence;
+        public List<Interactable.Action> actionOnClick;
+    }
+
+    [Serializable]
+    public class ButtonAction
+    {
+        public List<string> sentences;
+        public List<ButtonQuizz> buttonsInteractions;
+    }
+
+    public ButtonAction action;
+    public List<GameObject> allButtons;
+    public GameObject buttonPanel;
+    public TextMeshProUGUI question;
+
+    public void setUpPanel(int index)
+    {
+        question.text = action.sentences[index];
+        for (int i = 0; i < allButtons.Count; i++)
+        {
+            allButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = action.buttonsInteractions[index].sentence[i];
+            List<CinemaController.EventDescriptor> newEvents = new List<CinemaController.EventDescriptor>();
+            foreach (CinemaController.EventDescriptor ed in action.buttonsInteractions[index].actionOnClick[i].events)
+                newEvents.Add(ed);
+
+            allButtons[i].GetComponent<MCQButton>().events = newEvents;
+        }
+
+        buttonPanel.transform.DOScale(1f, 0.8f);
+    }
+}
---
+++ Assets/Scripts/MCQButton.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCQButton : MonoBehaviour
+{
+    public GameObject buttonsPanel;
+    public List<CinemaController.EventDescriptor> events;
+
+
+    public void ToPlayOnClick()
+    {
+        FindObjectOfType<CinemaController>().events = events;
+        buttonsPanel.transform.DOScale(0, 0.4f);
+        FindObjectOfType<CinemaController>().LaunchSequenceOfEvent();
+    }
+}

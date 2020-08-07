d0ceb5d0ccc5fb5e7e9fb88c6bc3b21d4bfe294c
Yohann DE <yohann.degli-esposti@epita.fr>
2020-08-07 15:54:24 +0200

    peauffiner: oh yeah.

---
+++ Assets/Scenes/IntermedScenes/AppearDisappear.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppearDisappear : MonoBehaviour
+{
+    public float onScene = 4f;
+    public TextMeshProUGUI text;
+    public int nextScene;
+
+    private void Start()
+    {
+        StartCoroutine(AppearDisapear());
+    }
+
+    private IEnumerator AppearDisapear()
+    {
+        while (text.color.a < 1f)
+        {
+            Color color = text.color;
+            color.a += 0.02f;
+            text.color = color;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        while (text.color.a > 0f)
+        {
+            Color color = text.color;
+            color.a -= 0.02f;
+            text.color = color;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
---
+++ Assets/Scripts/BasicButtonScripts.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BasicButtonScripts : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenPanel()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        panel.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        panel.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
